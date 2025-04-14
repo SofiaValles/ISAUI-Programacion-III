0f7393a2bee061fb4e1c491bcd36fb733d3d334e
Sofía Valles <sofiavalles.isaui@gmail.com>
2025-04-14 19:08:08 -0300

    avances en leet code

---
+++ Trabajo LeetCode/Problema LeetCode 28.py
@@ -0,0 +1,10 @@
+#Problema LeetCode 28
+
+#https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+
+#Dados dos strings hay que encontrar la primera ocurrencia de la segunda en la primera.
+#Si no se encuentra la segunda cadena en la primera, se devuelve -1.
+
+#Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
